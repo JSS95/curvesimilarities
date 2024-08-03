68c87f12e9b119351352bb90b1c42d35dc490e80
Jisoo Song <jeesoo9595@snu.ac.kr>
2024-08-04 00:35:10 +0900

    feat: implement frechet_matching
    Add and modify functions in _algorithms/fd.py

--- src/curvesimilarities/_algorithms/fd.py
+++ src/curvesimilarities/_algorithms/fd.py
@@ -123,68 +123,73 @@ def _critical_c(A, B, P1, P2):
 
 
 @njit(cache=True)
-def _reachable_boundaries(P, Q, eps):
+def _free_boundaries(P, Q, eps):
     p, q = len(P), len(Q)
+    BF = np.empty((p - 1, q, 2), dtype=np.float64)
+    LF = np.empty((p, q - 1, 2), dtype=np.float64)
 
-    B = np.empty((p - 1, q, 2), dtype=np.float64)
-    if p > 1:
-        start, end = _free_interval(P[0], P[1], Q[0], eps)
-        if start == 0:
-            B[0, 0] = [start, end]
+    for i in range(p - 1):
+        for j in range(q):
+            BF[i, j] = _free_interval(P[i], P[i + 1], Q[j], eps)
+    for i in range(p):
+        for j in range(q - 1):
+            LF[i, j] = _free_interval(Q[j], Q[j + 1], P[i], eps)
+
+    return BF, LF
+
+
+@njit(cache=True)
+def _reachable_boundaries(BF, LF, BR_out, LR_out):
+    # Leftmost boundary
+    if BF[0, 0, 0] == 0:
+        BR_out[0, 0] = BF[0, 0]
+    else:
+        BR_out[0, 0] = [NAN, NAN]
+    for i in range(1, BR_out.shape[0]):
+        if BF[i - 1, 0, 1] == 1 and BF[i, 0, 0] == 0:
+            BR_out[i, 0] = BF[i, 0]
         else:
-            B[0, 0] = [NAN, NAN]
-    for i in range(1, p - 1):
-        _, prev_end = B[i - 1, 0]
-        if prev_end == 1:
-            start, end = _free_interval(P[i], P[i + 1], Q[0], eps)
-            if start == 0:
-                B[i, 0] = [start, end]
-                continue
-        B[i, 0] = [NAN, NAN]
+            BR_out[i, 0] = [NAN, NAN]
 
-    L = np.empty((p, q - 1, 2), dtype=np.float64)
-    if q > 1:
-        start, end = _free_interval(Q[0], Q[1], P[0], eps)
-        if start == 0:
-            L[0, 0] = [start, end]
+    # Downmost boundary
+    if LF[0, 0, 0] == 0:
+        LR_out[0, 0] = LF[0, 0]
+    else:
+        LR_out[0, 0] = [NAN, NAN]
+    for j in range(1, LR_out.shape[1]):
+        if LF[0, j - 1, 1] == 1 and LF[0, j, 0] == 0:
+            LR_out[0, j] = LF[0, j]
         else:
-            L[0, 0] = [NAN, NAN]
-    for j in range(1, q - 1):
-        _, prev_end = L[0, j - 1]
-        if prev_end == 1:
-            start, end = _free_interval(Q[j], Q[j + 1], P[0], eps)
-            if start == 0:
-                L[0, j] = [start, end]
-                continue
-        L[0, j] = [NAN, NAN]
+            LR_out[0, j] = [NAN, NAN]
 
-    for i in range(1, p):
-        for j in range(1, q):
-            prevL_start, _ = L[i - 1, j - 1]
-            prevB_start, _ = B[i - 1, j - 1]
-            L_start, L_end = _free_interval(Q[j - 1], Q[j], P[i], eps)
-            B_start, B_end = _free_interval(P[i - 1], P[i], Q[j], eps)
-
-            if not np.isnan(prevB_start):
-                L[i, j - 1] = [L_start, L_end]
-            elif prevL_start <= L_end:
-                L[i, j - 1] = [max(prevL_start, L_start), L_end]
-            else:
-                L[i, j - 1] = [NAN, NAN]
+    # Propagate
+    for i in range(BR_out.shape[0]):
+        for j in range(LR_out.shape[1]):
+            prevB_start, _ = BR_out[i, j]
+            B_start, B_end = BF[i, j + 1]
+            prevL_start, _ = LR_out[i, j]
+            L_start, L_end = LF[i + 1, j]
 
             if not np.isnan(prevL_start):
-                B[i - 1, j] = [B_start, B_end]
+                BR_out[i, j + 1] = [B_start, B_end]
             elif prevB_start <= B_end:
-                B[i - 1, j] = [max(prevB_start, B_start), B_end]
+                BR_out[i, j + 1] = [max(prevB_start, B_start), B_end]
             else:
-                B[i - 1, j] = [NAN, NAN]
+                BR_out[i, j + 1] = [NAN, NAN]
 
-    return B, L
+            if not np.isnan(prevB_start):
+                LR_out[i + 1, j] = [L_start, L_end]
+            elif prevL_start <= L_end:
+                LR_out[i + 1, j] = [max(prevL_start, L_start), L_end]
+            else:
+                LR_out[i + 1, j] = [NAN, NAN]
+
+    return BR_out, LR_out
 
 
 @njit(cache=True)
 def _reachable_boundaries_1d(P, Q, eps):
-    # Equivalent to _reachable_boundaries, but keep 1d array instead of 2d.
+    # Keep 1d array instead of 2d, and do free + reachable in one-shot.
     # Memory efficient, but cannot do backtracking.
     p, q = len(P), len(Q)
     B = np.empty((1, 2), dtype=np.float64)
@@ -241,27 +246,31 @@ def _reachable_boundaries_1d(P, Q, eps):
 @njit(cache=True)
 def _free_interval(A, B, P, eps):
     # resulting interval is always in [0, 1] or is [nan, nan].
-    coeff1 = B - A
-    coeff2 = A - P
-    a = np.dot(coeff1, coeff1)
-    c = np.dot(coeff2, coeff2) - eps**2
-    if a == 0:  # degenerate case
-        if c > 0:
-            interval = [NAN, NAN]
-        else:
-            interval = [np.float64(0), np.float64(1)]
-        return interval
-    b = 2 * np.dot(coeff1, coeff2)
+    start = end = NAN
+
+    pa = A - P
+    pb = B - P
+    if np.dot(pa, pa) <= eps**2:
+        start = np.float64(0)
+    if np.dot(pb, pb) <= eps**2:
+        end = np.float64(1)
+
+    ab = B - A
+    a = np.dot(ab, ab)
+    if a == 0:
+        return [start, end]
+
+    b = 2 * np.dot(ab, pa)
+    c = np.dot(pa, pa) - eps**2
     Det = b**2 - 4 * a * c
-    if Det < 0:
-        interval = [NAN, NAN]
-    else:
-        start = max((-b - Det**0.5) / 2 / a, np.float64(0))
-        end = min((-b + Det**0.5) / 2 / a, np.float64(1))
-        if start > 1 or end < 0:
-            start = end = NAN
-        interval = [start, end]
-    return interval
+    if Det >= 0:
+        if np.isnan(start):  # pa.pa > eps ** 2
+            start = (-b - Det**0.5) / 2 / a
+        if np.isnan(end):  # pb.pb > eps ** 2
+            end = (-b + Det**0.5) / 2 / a
+    if start > 1 or end < 0:
+        start = end = NAN
+    return [start, end]
 
 
 @njit(cache=True)
@@ -358,12 +367,14 @@ def _fd_params(P, Q, rel_tol, abs_tol):
     THRES = np.max(crit_a)
     crit = np.sort(np.concatenate((crit_a[crit_a >= THRES], crit_b[crit_b >= THRES])))
     start, end = 0, len(crit) - 1
-    B, L = _reachable_boundaries(P, Q, crit[start])
+    B, L = _free_boundaries(P, Q, crit[start])
+    B, L = _reachable_boundaries(B, L, B, L)
     if B[-1, -1, 1] == 1 or L[-1, -1, 1] == 1:
         end = start
     while end - start > 1:
         mid = (start + end) // 2
-        B, L = _reachable_boundaries(P, Q, crit[mid])
+        B, L = _free_boundaries(P, Q, crit[mid])
+        B, L = _reachable_boundaries(B, L, B, L)
         if B[-1, -1, 1] == 1 or L[-1, -1, 1] == 1:
             end = mid
         else:
@@ -375,7 +386,8 @@ def _fd_params(P, Q, rel_tol, abs_tol):
         mid = (e1 + e2) / 2
         if (mid - e1 < EPSILON) or (e2 - mid < EPSILON):
             break
-        B, L = _reachable_boundaries(P, Q, mid)
+        B, L = _free_boundaries(P, Q, mid)
+        B, L = _reachable_boundaries(B, L, B, L)
         if B[-1, -1, 1] == 1 or L[-1, -1, 1] == 1:
             e2 = mid
         else:
